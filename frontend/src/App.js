9c24042a7a0ea41561b69da045515e60b98265f6
asigba <alexsigba@gmail.com>
2025-05-11 21:00:02 -0400

    fixed navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { use } from 'react';
-import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import UserProfile from './pages/UserProfile';
 import Products from './pages/Products'; 
@@ -9,9 +9,9 @@ import Checkout from './pages/Checkout';
 import Login from './pages/Login'; 
 import Navbar from './pages/Navbar';
 import './App.css';
+import { useLocation } from 'react-router-dom';
 
-
-function App() {
+function AppContent() {
   const location = useLocation();
 
   const hideNavbarRoutes = ['/login','/register']
@@ -20,7 +20,6 @@ function App() {
   return (
     <>
     {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
-    <Router>
         <div className='store'>
           <Routes>
             <Route path='/' element={<Home />} />
@@ -32,9 +31,18 @@ function App() {
             <Route path='/products/:productId' element={<ProductDetails />} />         
           </Routes>  
         </div>
-    </Router>
+      
     </>
   );
 }
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
 
 export default App;
