baa3e953f998ec261ea45d315e71903d0826082b
asigba <alexsigba@gmail.com>
2025-05-15 13:53:13 -0400

    adding user feature

---
+++ frontend/src/pages/AuthContext.jsx
@@ -0,0 +1,5 @@
+import React, {createContext, useState} from 'react';
+
+export default function AuthContext({children}) {
+    
+}
--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css'; 
 
 export default function Navbar() {
+    const [isSignIn, setIsSignedIn] = useState('false');
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">
@@ -11,7 +13,6 @@ export default function Navbar() {
             <ul className="navbar-links">
                 <li><Link to="/products">Products</Link></li>
                 <li><Link to="/carts">Cart</Link></li>
-                <li><Link to="/login">Login</Link></li>
             </ul>
 
         </nav>
