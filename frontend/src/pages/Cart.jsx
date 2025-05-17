d83f23e3f8f7250340eea9284b08a0d97c5eea40
asigba <alexsigba@gmail.com>
2025-05-17 14:17:33 -0400

    reorganizing the directories

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useCart } from './CartContext';
 import './Cart.css';
+import { Link } from 'react-router-dom';
 
 export default function Cart(){
     const {cartItems} = useCart();
